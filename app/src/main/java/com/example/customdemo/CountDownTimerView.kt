2bb82e605a673b07e4cd51994d1b52ec6fd725d1
enes selçuk <enesselcu25@gmail.com>
2023-10-16 11:11:24 +0300

    initial commit

---
+++ app/src/main/java/com/example/customdemo/CountDownTimerView.kt
@@ -0,0 +1,100 @@
+package com.example.customdemo
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
+import android.os.CountDownTimer
+import android.util.AttributeSet
+import android.view.View
+
+class CountDownTimerView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+
+    private val circlePaint: Paint = Paint()
+    private val progressPaint: Paint = Paint()
+    private val textPaint: Paint = Paint()
+    private var countDownTimer: CustomCountDownTimer? = null
+    private var totalTimeInMillis: Long = 0
+
+    init {
+        circlePaint.color = Color.RED
+        circlePaint.style = Paint.Style.STROKE
+        circlePaint.strokeWidth = 20f
+        circlePaint.isAntiAlias = true
+
+        progressPaint.color = Color.LTGRAY
+        progressPaint.style = Paint.Style.STROKE
+        progressPaint.strokeWidth = 20f
+        progressPaint.isAntiAlias = true
+
+        textPaint.color = Color.BLACK
+        textPaint.style = Paint.Style.FILL
+        textPaint.textSize = 80f
+        textPaint.textAlign = Paint.Align.CENTER
+        textPaint.isAntiAlias = true
+    }
+
+    fun startTimer(totalTimeInMillis: Long) {
+        val minutes = totalTimeInMillis * 60 * 1000
+        this.totalTimeInMillis = minutes
+
+        countDownTimer = CustomCountDownTimer(minutes, 1000) {
+            invalidate()
+        }
+
+        countDownTimer?.start()
+    }
+
+    private class CustomCountDownTimer(
+        millisInFuture: Long,
+        countDownInterval: Long,
+        private val onTick: () -> Unit
+    ) : CountDownTimer(millisInFuture, countDownInterval) {
+
+        private var timePassed: Long = 0
+        private var millisInFuture2 = millisInFuture
+
+        override fun onTick(millisUntilFinished: Long) {
+            timePassed = millisInFuture2 - millisUntilFinished
+            onTick.invoke()
+        }
+
+        override fun onFinish() {
+            timePassed = millisInFuture2
+            onTick.invoke()
+        }
+
+        fun getTimeRemaining(): Long {
+            return millisInFuture2 - timePassed
+        }
+    }
+
+    @SuppressLint("DrawAllocation")
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        val centerX = width / 2f
+        val centerY = height / 2f
+        val radius = centerX.coerceAtMost(centerY) - 10f
+
+        // Çemberi çiz
+        val oval = RectF(centerX - radius, centerY - radius, centerX + radius, centerY + radius)
+        canvas.drawArc(oval, -90f, 360f, false, circlePaint)
+
+        // Progress'i çiz
+        val timeRemaining = countDownTimer?.getTimeRemaining() ?: totalTimeInMillis
+        val progressRatio = 1.0 - (timeRemaining.toDouble() / totalTimeInMillis.toDouble()).coerceAtMost(1.0)
+        val sweepAngle = (360 * progressRatio).toFloat()
+        canvas.drawArc(oval, -90f, sweepAngle, false, progressPaint)
+
+        // Sayacı çiz
+        val minutes = (timeRemaining / 1000 / 60).toInt()
+        val seconds = (timeRemaining / 1000 % 60).toInt()
+        val timeRemainingText = String.format("%02d:%02d", minutes, seconds)
+        canvas.drawText(timeRemainingText, centerX, centerY + textPaint.textSize / 4, textPaint)
+    }
+}
+
+
---
+++ app/src/main/java/com/example/customdemo/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.customdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var countDownTimerView: CountDownTimerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        countDownTimerView = findViewById(R.id.countTimeDowner)
+
+        countDownTimerView.startTimer(1)
+    }
+}
