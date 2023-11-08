969f485e1100af0a6a3abb9fafc86c1ab8eed8f1
Enes Selçuk <enesselcu25@gmail.com>
2023-11-08 19:46:49 +0300

    Update CountDownTimerView.kt

--- app/src/main/java/com/example/customdemo/CountDownTimerView.kt
+++ app/src/main/java/com/example/customdemo/CountDownTimerView.kt
@@ -12,11 +12,13 @@ import android.view.View
 
 class CountDownTimerView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
-    private val circlePaint: Paint = Paint()
+  private val circlePaint: Paint = Paint()
     private val progressPaint: Paint = Paint()
     private val textPaint: Paint = Paint()
     private var countDownTimer: CustomCountDownTimer? = null
     private var totalTimeInMillis: Long = 0
+
+    var onFinishListener: (() -> Unit)? = null
 
     init {
         circlePaint.color = Color.RED
@@ -35,22 +37,26 @@ class CountDownTimerView(context: Context, attrs: AttributeSet) : View(context, 
         textPaint.textAlign = Paint.Align.CENTER
         textPaint.isAntiAlias = true
     }
-
-    fun startTimer(totalTimeInMillis: Long) {
+fun startTimer(totalTimeInMillis: Long) {
         val minutes = totalTimeInMillis * 1000
         this.totalTimeInMillis = minutes
 
-        countDownTimer = CustomCountDownTimer(minutes, 1000) {
-            invalidate()
-        }
+        countDownTimer = CustomCountDownTimer(minutes, 1000,
+            onFinish = { onFinishListener?.invoke() }, onTick = { invalidate() })
 
         countDownTimer?.start()
     }
 
+    fun stopTimer() {
+        countDownTimer?.cancel()
+    }
+
+
     private class CustomCountDownTimer(
         millisInFuture: Long,
         countDownInterval: Long,
-        private val onTick: () -> Unit
+        private val onTick: () -> Unit,
+        private val onFinish: () -> Unit,
     ) : CountDownTimer(millisInFuture, countDownInterval) {
 
         private var timePassed: Long = 0
@@ -64,19 +70,13 @@ class CountDownTimerView(context: Context, attrs: AttributeSet) : View(context, 
         override fun onFinish() {
             timePassed = millisInFuture2
             onTick.invoke()
+            onFinish.invoke()
         }
 
         fun getTimeRemaining(): Long {
             return millisInFuture2 - timePassed
         }
     }
-
-
-
-
-
-
-
 
     @SuppressLint("DrawAllocation")
     override fun onDraw(canvas: Canvas) {
@@ -86,30 +86,24 @@ class CountDownTimerView(context: Context, attrs: AttributeSet) : View(context, 
         val centerY = height / 2f
         val radius = centerX.coerceAtMost(centerY) - 10f
 
-        // Çemberi çiz
         val oval = RectF(centerX - radius, centerY - radius, centerX + radius, centerY + radius)
         canvas.drawArc(oval, -90f, 360f, false, circlePaint)
 
-        // Progress'i çiz
         val timeRemaining = countDownTimer?.getTimeRemaining() ?: totalTimeInMillis
         val seconds = (timeRemaining / 1000 % 60).toFloat()
         val totalSeconds = (totalTimeInMillis / 1000).toFloat()
 
-        // Geriye doğru akışlı progress
         val sweepAngle = 360 * (1 - seconds / totalSeconds)
         canvas.drawArc(oval, -90f, sweepAngle, false, progressPaint)
 
-        // Sayacı çiz
         val timeRemainingText = seconds.toInt().toString()
         canvas.drawText(timeRemainingText, centerX, centerY + textPaint.textSize / 4, textPaint)
 
-        // Eğer saniye 1'e düştüyse, bir sonraki güncelleme için zamanlanmış bir işlem yap
         if (seconds == 1f) {
             postDelayed({
                 invalidate()
             }, 1000)
         } else {
-            // Başka bir çizim tetiklemek için görünümü geçersiz kılma
             postInvalidateOnAnimation()
         }
     }
